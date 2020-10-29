302acbff328111639918fd364ebb555a42974e92
Théo Roton <theo.roton@atilf.fr>
2020-10-29 15:23:03 +0100

    test

--- mazegame/src/main/java/com/gameric/mazegame/start/Main.java
+++ mazegame/src/main/java/com/gameric/mazegame/start/Main.java
@@ -6,18 +6,20 @@ import com.gameric.mazegame.model.PacmanGame;
 import com.gameric.mazegame.model.PacmanPainter;
 
 /**
- * lancement du moteur avec le jeu
+ * Lancement du moteur avec le jeu
  */
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		// creation du jeu particulier et de son afficheur
+		//Création du jeu
 		PacmanGame game = new PacmanGame("helpFilePacman.txt");
+		//Création de l'afficheur
 		PacmanPainter painter = new PacmanPainter();
+		//Création du contrôleur
 		PacmanController controller = new PacmanController();
 
-		// classe qui lance le moteur de jeu generique
+		//Lancement du moteur graphique
 		GameEngineGraphical engine = new GameEngineGraphical(game, painter, controller);
 		engine.run();
 	}
