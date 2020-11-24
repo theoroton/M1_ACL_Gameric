42700531313195268fa14ebed01f29ba94b3a381
Théo Roton <theo.roton@atilf.fr>
2020-11-24 20:37:31 +0100

    Javadoc

--- mazegame/src/main/java/com/gameric/mazegame/model/objets/ObjetMystere.java
+++ mazegame/src/main/java/com/gameric/mazegame/model/objets/ObjetMystere.java
@@ -1,46 +1,69 @@
 package com.gameric.mazegame.model.objets;
 
-import java.util.Random;
-
 import com.gameric.mazegame.model.personnage.Personnage;
 
+/**
+ * 
+ * @author Théo Roton
+ * Classe ObjetMystere
+ */
 public class ObjetMystere extends Objet {
 
+	/**
+	 * Constructeur de l'objet mystère;
+	 * l'objet mystère est représenté par un "?"
+	 */
 	public ObjetMystere() {
 		super("?");
 	}
 
-	@Override
+	/**
+	 * Méthode qui effectue l'effet de l'objet sur le personnage
+	 * quand il le ramasse.
+	 * On va modifier positivement ou négativement une statistique
+	 * aléatoire du personnage.
+	 * @param p : personnage sur lequel l'effectuer l'effet.
+	 */
 	public void effetObjet(Personnage p) {
-		int rand = (int) (Math.random() * (9 - 1));
+		//On prend un entier compris entre 0 et 7
+		int rand = (int) (Math.random() * (8 - 0));
 		
+		//Effet possibles
 		switch (rand) {
-		case 1:
+		//On réduit les pvMax de 4
+		case 0:
 			p.setVieMax(p.getVieMax() - 4);
 			p.setPointsVie(p.getPointsVie() - 4);
 			break;
-		case 2:
+		//On augmente les pvMax de 4
+		case 1:
 			p.setVieMax(p.getVieMax() + 4);
 			p.setPointsVie(p.getPointsVie() + 4);
 			break;
-		case 3:
+		//On réduit les pv de 6
+		case 2:
 			p.setPointsVie(p.getPointsVie() - 6);
 			break;
-		case 4:
+		//On augmente les points de vie de 6
+		case 3:
 			p.setPointsVie(p.getPointsVie() + 6);
 			break;
-		case 5:
+		//On réduit les dégâts de 3
+		case 4:
 			p.setDegats(p.getDegats() - 3);
 			break;
-		case 6:
+		//On augmente les dégâts de 3
+		case 5:
 			p.setDegats(p.getDegats() + 3);
 			break;
-		case 7:
+		//On réduit la portée de 1 (si elle est supérieure à 1)
+		case 6:
 			if (p.getPortee() > 1) {
 				p.setPortee(p.getPortee() - 1);
 			}
 			break;
-		case 8:
+		//On augmente la portée de 1
+		case 7:
 			p.setPortee(p.getPortee() + 1);
 			break;
 		}
