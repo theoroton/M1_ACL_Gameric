7c0a9037164fa7202d586ceb3f39d29ef93f9065
Maëva <maeva.touchet@outlook.fr>
2020-12-17 22:08:05 +0100

    //

--- mazegame/src/main/java/com/gameric/mazegame/model/personnage/Archer.java
+++ mazegame/src/main/java/com/gameric/mazegame/model/personnage/Archer.java
@@ -15,6 +15,32 @@ public class Archer extends Personnage{
 		degats = 5;
 		portee = 5;
 		compteur = 0;
+
+		for (int i = 0; i < 9; i++ ) {
+			walkingDown[i] = new Sprite().getSprite(i, 10, this.getClass());
+			walkingLeft[i] = new Sprite().getSprite(i, 9, this.getClass());
+			walkingRight[i] = new Sprite().getSprite(i, 11, this.getClass());
+			walkingUp[i] = new Sprite().getSprite(i, 8, this.getClass());
+		}
+		stand[0] = new Sprite().getSprite(0, 2, this.getClass());
+
+		for (int i = 0; i < 12; i++) {
+			attaqueUp[i] = new Sprite().getSprite(i, 16, this.getClass());
+			attaqueDown[i] = new Sprite().getSprite(i, 18, this.getClass());
+			attaqueLeft[i] = new Sprite().getSprite(i, 17, this.getClass());
+			attaqueRight[i] = new Sprite().getSprite(i, 19, this.getClass());
+		}
+
+		walkingU = new Animation(walkingUp, 10);
+		walkingD = new Animation(walkingDown, 10);
+		walkingL = new Animation(walkingLeft, 10);
+		walkingR = new Animation(walkingRight, 10);
+		standing = new Animation(stand, 10);
+
+		attaqueU = new Animation(attaqueUp, 10);
+		attaqueD = new Animation(attaqueDown, 10);
+		attaqueL = new Animation(attaqueLeft, 10);
+		attaqueR= new Animation(attaqueRight, 10);
 	}
 	//Méthodes
 	/**
--- mazegame/src/main/java/com/gameric/mazegame/model/personnage/Epeiste.java
+++ mazegame/src/main/java/com/gameric/mazegame/model/personnage/Epeiste.java
@@ -12,6 +12,32 @@ public class Epeiste extends Personnage{
 		pointsVie = vieMax = 30;
 		degats = 10;
 		portee = 1;
+
+		for (int i = 0; i < 9; i++ ) {
+			walkingDown[i] = new Sprite().getSprite(i, 10, this.getClass());
+			walkingLeft[i] = new Sprite().getSprite(i, 9, this.getClass());
+			walkingRight[i] = new Sprite().getSprite(i, 11, this.getClass());
+			walkingUp[i] = new Sprite().getSprite(i, 8, this.getClass());
+		}
+		stand[0] = new Sprite().getSprite(0, 2, this.getClass());
+
+		for (int i = 0; i < 12; i++) {
+			attaqueUp[i] = new Sprite().getSprite(i, 22, this.getClass());
+			attaqueDown[i] = new Sprite().getSprite(i, 24, this.getClass());
+			attaqueLeft[i] = new Sprite().getSprite(i, 23, this.getClass());
+			attaqueRight[i] = new Sprite().getSprite(i, 25, this.getClass());
+		}
+
+		walkingU = new Animation(walkingUp, 10);
+		walkingD = new Animation(walkingDown, 10);
+		walkingL = new Animation(walkingLeft, 10);
+		walkingR = new Animation(walkingRight, 10);
+		standing = new Animation(stand, 10);
+
+		attaqueU = new Animation(attaqueUp, 10);
+		attaqueD = new Animation(attaqueDown, 10);
+		attaqueL = new Animation(attaqueLeft, 10);
+		attaqueR= new Animation(attaqueRight, 10);
 	}
 	//Méthodes
 
